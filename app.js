5fa3f02130ca410d8752a70bff56e9f6866f3c27
saloni <saloni18heatabit@gmail.com>
2026-07-20 01:12:07 +0530

    error handling

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ app.use(express.json());
 app.use("/", userRoutes);
 
 app.use((err, req, res, next)=>{
+    console.error("Error in User Service:", err);
     const statusCode =  err.statusCode || 500;
     const message = err.message || 'Internal Server Error in User Service';
-})
+    res.status(statusCode).json({ error: message });
+});
 
 module.exports = app;
